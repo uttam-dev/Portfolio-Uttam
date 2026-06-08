1af9df2e310a91067c63afda704aa818dccb34e8
Uttam Prajapati <uttam.prajapati@rysun.com>
2026-06-08 21:46:54 +0530

    feat: initialize React project with Vite, ESLint, and Prettier configuration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="portfolio-container">
+      <div className="title-wrapper">
+        <h1 className="name">Uttam prajapati</h1>
+        <span className="subtitle">Turn coffee into code</span>
+      </div>
+    </main>
+  )
+}
+
+export default App
